72edd5d51e0d77b197abfde44ccf22b6b974e80d
IsabellaRocha <irocha00@stuy.edu>
2019-02-07 11:35:15 -0500

    Wrote first parts of add and remove and constructor

---
+++ QueenBoard.java
@@ -0,0 +1,22 @@
+public class QueenBoard {
+  private int[][] board;
+  public QueenBoard(int size) {
+    board = new int[size][size];
+    for (int idx = 0; idx < board.length; idx++) {
+      for (int x = 0; x < board[idx].length; x++) {
+        board[idx][x] = 0;
+      }
+    }
+  }
+  private boolean addQueen(int r, int c) {
+    if (board[r][c] == 0) {
+      board[r][c] = -1;
+      return true;
+    }
+    return false;
+  }
+  private boolean removeQueen(int r, int c) {
+    board[r][c] = 0;
+    return true;
+  }
+}
